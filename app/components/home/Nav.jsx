df74913f323de69e667789d412bacc762e2b4606
WaiYan MoeMyint <wymoemyint4523@gmail.com>
2024-07-30 22:03:41 +0630

    update mobile ui

--- app/components/home/Nav.jsx
+++ app/components/home/Nav.jsx
@@ -55,23 +55,23 @@ const Nav = () => {
         </div>
       </nav>
       {menu && (
-        <div className="px-4 py-2.5 mt-4 bg-slate-100 h-full inset-0">
+        <div className="px-4 py-2.5  inset-0 overflow-hidden shadow-md">
           <div className="flex flex-col gap-4 w-full">
             <Link
               href="#features"
-              className="hover:bg-slate-200 py-1 px-2 rounded-md transition-all"
+              className="hover:bg-slate-200 py-2 px-2 rounded-md transition-all"
             >
               Features
             </Link>
             <Link
               href="#pricing"
-              className="hover:bg-slate-200 py-1 px-2 rounded-md transition-all"
+              className="hover:bg-slate-200 py-2 px-2 rounded-md transition-all"
             >
               Pricing
             </Link>
             <Link
               href="#student"
-              className="hover:bg-slate-200 py-1 px-2 rounded-md transition-all"
+              className="hover:bg-slate-200 py-2 px-2 rounded-md transition-all"
             >
               For Student
             </Link>
