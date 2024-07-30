851da14154c43a930bd83bb2ac3e88115b7ff862
WaiYan MoeMyint <wymoemyint4523@gmail.com>
2024-07-30 22:27:38 +0630

    add pointer events

--- app/components/home/Profile.jsx
+++ app/components/home/Profile.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Profile = () => {
   return (
     <div className="mt-12 px-4 py-2.5">
-      <div className="w-full flex items-end justify-center">
+      <div className="w-full flex items-end justify-center pointer-events-none">
         <Image src="/banner.png" alt="banner" width={1000} height={1000} />
       </div>
       <div className  = "profile-title mt-8 w-full flex flex-col gap-4 items-center justify-center text-center ">
