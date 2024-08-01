45c3a05ccfda06a65e35f7481be785b46e6ba324
WaiYan MoeMyint <wymoemyint4523@gmail.com>
2024-08-01 16:04:06 +0630

    Update sidebar icons

--- app/components/tasks/General.jsx
+++ app/components/tasks/General.jsx
@@ -5,25 +5,25 @@ import Link from "next/link";
 const General = () => {
   const [hover, setHover] = useState(1);
   return (
-    <div className="mt-5 px-1.5">
+    <div className="mt-5 px-1">
       <div className="general_title">
         <h3 className="text-slate-500 text-sm">General</h3>
       </div>
       <div className="general_list flex flex-col gap-6 mt-4">
         <div onClick = {()=> setHover(1)} className={`flex items-center gap-2 py-2.5 px-4 rounded-lg cursor-pointer ${hover == 1 ? "bg-white shadow-sm transition-all" : "bg-transparent"}`}>
-          <Image src="/home_simple.svg" alt="home" width={20} height={20} />
+          <Image src={hover === 1 ? "/hover_home.svg" : "/home_simple.svg" } alt="home" width={20} height={20} />
           <p className = "text-sm">Todo</p>
         </div>
         <div onClick = {()=> setHover(2)} className={`flex items-center gap-2 py-2.5 px-4 rounded-lg cursor-pointer ${hover == 2 ? "bg-white shadow-sm transition-all" : "bg-transparent"}`} >
-          <Image src="/mail.svg" alt="home" width={20} height={20} />
+          <Image src={hover === 2 ? "/hover_mail.svg" : "/mail.svg" } alt="home" width={20} height={20} />
           <p className = "text-sm">Inbox</p>
         </div>
         <div onClick = {()=> setHover(3)} className={`flex items-center gap-2 py-2.5 px-4 rounded-lg cursor-pointer ${hover == 3 ? "bg-white shadow-sm transition-all" : "bg-transparent"}`}>
-          <Image src="/calendar.svg" alt="home" width={20} height={20} />
+          <Image src={hover === 3 ? "/hover_calendar.svg" : "/calendar.svg" } alt="home" width={20} height={20} />
           <p className = "text-sm">Calendar</p>
         </div>
         <div onClick = {()=> setHover(4)} className={`flex items-center gap-2 py-2.5 px-4 rounded-lg cursor-pointer ${hover == 4 ? "bg-white shadow-sm transition-all" : "bg-transparent"}`}>
-          <Image src="/trash.svg" alt="home" width={20} height={20} />
+          <Image src={hover === 4 ? "/hover_trash.svg" : "/trash.svg" } width={20} height={20} />
           <p className = "text-sm">Trash</p>
         </div>
       </div>
