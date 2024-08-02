2dcc9e9fa924fc57154b11802d2cf926d0e811af
WaiYan MoeMyint <wymoemyint4523@gmail.com>
2024-08-02 09:57:53 +0630

    Add animation

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -1,33 +1,36 @@
 'use client';
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { SignOutButton, useUser } from "@clerk/nextjs";
 import Image from "next/image";
 import General from "./tasks/General";
 import Board from "./tasks/Board";
 import Ads from "./tasks/Ads";
+
 const Sidebar = () => {
   const { user } = useUser();
-  console.log(user);
   const [column, setColumn] = useState(false);
+
   return (
-    <div className = "bg-blue-500 py-2 px-2.5 sidebar_content w-60">
+    <div className={`py-2 px-2.5 sidebar_content ${!column ? "w-60 transition-all" : "w-36 transition-all"} transition-all duration-300`}>
       <div className="sidebar">
-        <div className="sidbar_title cursor-pointer flex py-1.5 px-1.5 bg-white gap-2.5 items-center justify-between  shadow-sm rounded-lg">
+        <div className="sidbar_title cursor-pointer flex py-1.5 px-1.5 bg-white gap-2.5 items-center justify-between shadow-sm rounded-lg">
           <div className="flex gap-1 items-center">
             <Image
               src={`${user?.imageUrl ? user?.imageUrl : "/avatar.png"}`}
               alt="avatar"
               width={35}
               height={35}
-              className = "rounded-full block"
+              className="rounded-full block"
             />
-            <p className = "text-sm  font-bold user_title select-none">{user?.fullName ? user.fullName : "BumBum's Space"}</p>
+            <p className={`text-sm font-bold user_title select-none ${!column ? "block transition-all" : "hidden transition-all"}`}>{user?.fullName ? user.fullName : "BumBum's Space"}</p>
           </div>
-          <Image src="/column.svg" alt="shield" width={20} height={20} />
+          <div onClick={() => setColumn(prev => !prev)}>
+            <Image src="/column.svg" alt="shield" width={20} height={20} />
+          </div>
         </div>
 
         <General />
-        <Board/>
+        <Board />
         <Ads />
         <SignOutButton>SignOut</SignOutButton>
       </div>
--- app/components/tasks/Board.jsx
+++ app/components/tasks/Board.jsx
@@ -4,34 +4,58 @@ import Image from "next/image";
 import Link from "next/link";
 import { BumBumContext } from "@/app/context/BumBum";
 const Board = () => {
-  const {hover, setHover} = useContext(BumBumContext);
-    
+  const { hover, setHover } = useContext(BumBumContext);
+
   return (
     <div className="mt-5 px-1">
       <div className="general_title  py-2 flex items-center rounded-md px-1 justify-between gap-1">
         <h3 className="text-slate-500 text-sm">Boards</h3>
-        <div className = "hover:bg-white shadow-sm  rounded-full cursor-pointer">
+        <div className="hover:bg-white shadow-sm  rounded-full cursor-pointer">
           <Image src="/plus.svg" alt="create" width={20} height={20} />
         </div>
       </div>
       <div className="general_list flex flex-col gap-6 mt-4">
         <div
           onClick={() => setHover(5)}
-          className={`flex items-center gap-2 py-2.5 px-4 rounded-lg cursor-pointer ${
+          className={`flex items-center justify-between gap-2 py-2.5 px-4 rounded-lg cursor-pointer ${
             hover === 5 ? "bg-white shadow-sm transition-all" : "bg-transparent"
           }`}
         >
-          <div className = "bg-red-500 w-2 h-2 rounded-full"></div>
-          <p className="text-sm">Skincare Routine</p>
+          <div className="flex items-center gap-2">
+            <div className="bg-red-500 w-2 h-2 rounded-full"></div>
+            <p className="text-sm">Skincare Routine</p>
+          </div>
+
+          <div className="flex items-center gap-1">
+            <Image
+              src="/dots.svg"
+              alt="dots"
+              className={`cursor-pointer ${hover === 5 ? "block" :"hidden"}`}
+              width={20}
+              height={20}
+            />
+          </div>
         </div>
         <div
-          onClick={() => setHover(7)}
-          className={`flex items-center gap-2 py-2.5 px-4 rounded-lg cursor-pointer ${
-            hover === 7 ? "bg-white shadow-sm transition-all" : "bg-transparent"
+          onClick={() => setHover(6)}
+          className={`flex items-center justify-between gap-2 py-2.5 px-4 rounded-lg cursor-pointer ${
+            hover === 6 ? "bg-white shadow-sm transition-all" : "bg-transparent"
           }`}
         >
-          <div className = "bg-teal-500 w-2 h-2 rounded-full"></div>
-          <p className="text-sm">Workout</p>
+           <div className="flex items-center gap-2">
+            <div className="bg-teal-500 w-2 h-2 rounded-full"></div>
+            <p className="text-sm">Skincare Routine</p>
+          </div>
+
+          <div className="flex items-center gap-1">
+            <Image
+              src="/dots.svg"
+              alt="dots"
+              className={`cursor-pointer ${hover === 6 ? "block" :"hidden"}`}
+              width={20}
+              height={20}
+            />
+          </div>
         </div>
       </div>
     </div>
