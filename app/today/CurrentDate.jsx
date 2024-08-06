957fd813c57f2c89a9696b1afe38ca71c6411674
WaiYan MoeMyint <wymoemyint4523@gmail.com>
2024-08-06 16:17:30 +0630

    Add dialog box

--- app/today/CurrentDate.jsx
+++ app/today/CurrentDate.jsx
@@ -1,22 +1,76 @@
-'use client';
-import React from 'react';
-import { Button } from '@/components/ui/button';
-import { CirclePlus, Plus } from 'lucide-react';
+"use client";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { CirclePlus, Plus } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 const CurrentDate = () => {
-    const date = new Date();
+  const date = new Date();
   return (
     <div>
-        <div className = "task_list flex mt-4 items-center justify-between">
-             <div className = "greeting">
-                <h3 className = "text-[#352e5d] font-semibold ">Today, {date.getDate()} {date.toLocaleDateString('en-US', { month: 'short' })}</h3>
-             </div>
-             <div className = "task_button">
-                <Button className= "w-full nav_primary_btn hover:bg-indigo-700 transition-all  py-1.5 px-2.5 rounded-md">
-                  <Plus className = "mr-1" /> New Task</Button>
-             </div>
+      <div className="task_list flex mt-4 items-center justify-between">
+        <div className="greeting">
+          <h3 className="text-[#352e5d] font-semibold ">
+            Today, {date.getDate()}{" "}
+            {date.toLocaleDateString("en-US", { month: "short" })}
+          </h3>
         </div>
+        <Dialog className="task_button">
+          <DialogTrigger asChild>
+            <Button className=" nav_primary_btn hover:bg-indigo-700 transition-all  py-1.5 px-2.5 rounded-md">
+              <Plus className="mr-1" /> New Task
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Add New Task</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="w-full flex flex-col gap-4">
+                <div>
+                  <Label htmlFor="name" className="text-right">
+                    Task Title
+                  </Label>
+                  <Input
+                    id="name"
+                    defaultValue="e.g. Going to book shop"
+                    className="col-span-3 mt-2 text-slate-700 text-sm"
+                  />
+                </div>
+              </div>
+              <div className="w-full flex flex-col  gap-4">
+              <div>
+                  <Label htmlFor="name" className="text-right">
+                    Task Description
+                  </Label>
+                  <Input
+                    id="name"
+                    defaultValue="e.g. to buy comic books"
+                    className="col-span-3 mt-2 text-slate-700 text-sm"
+                  />
+                </div>
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit" className = "bg-indigo-600 w-full hover:bg-indigo-800">Create Task</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CurrentDate
+export default CurrentDate;
--- app/today/Tasklist.jsx
+++ app/today/Tasklist.jsx
@@ -2,19 +2,25 @@ import React from "react";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Timer } from "lucide-react";
 import Image from "next/image";
+
 const Tasklist = () => {
   const date = new Date();
-  console.log(date.getHours());
   return (
     <div className="mt-4">
-      <div className="tasks_content  w-96 py-2 cursor-pointer px-4 flex gap-2 items-center rounded-xl">
+      <div className="tasks_content  w-96 py-2 cursor-pointer px-4 flex gap-4 items-center rounded-xl">
         <Checkbox />
         <div className="">
-          <h3>Learn a Python</h3>
-          <p>watch tutorial on youtube</p>
-          <div className = "time flex gap-1 items-center">
-            <Image src="/time.svg" alt="time" width={20} height={20} />
-            <p>Tue</p>
+          <h3 className="text-[#352e5d] mb-0.5">Learn a Python</h3>
+          <p className="text-sm text-slate-500">Watch tutorial on youtube</p>
+          <div className="time flex gap-2 text-sm items-center mt-2.5 ">
+            <div className="flex items-center gap-1">
+              <Image src="/time.svg" alt="time" width={15} height={15} />
+              <p className="text-slate-600">Tue</p>
+            </div>
+            <div className="flex items-center gap-1">
+              <Image src="/comment.svg" alt="time" width={15} height={15} />
+              <p className="text-slate-600">0 comment</p>
+            </div>
           </div>
         </div>
       </div>
