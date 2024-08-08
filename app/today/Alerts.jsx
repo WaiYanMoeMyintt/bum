7af526369cbb646b9ba36e1427838c5160063411
WaiYan MoeMyint <wymoemyint4523@gmail.com>
2024-08-08 11:46:01 +0630

    Update Delete UI

---
+++ app/today/Alerts.jsx
@@ -0,0 +1,35 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+
+export default function AlertDialogDemo({ onClose }) {
+  return (
+    <AlertDialog open={true} onOpenChange={onClose}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your task
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={onClose}>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            className="bg-red-500 text-white hover:bg-red-600"
+            onClick={onClose}
+          >
+            Delete
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
--- app/today/TaskActions.jsx
+++ app/today/TaskActions.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Cloud,
   CreditCard,
@@ -15,85 +16,98 @@ import {
   Users,
   Pencil,
   Trash2,
-} from "lucide-react";
-import Image from "next/image";
+} from 'lucide-react';
+import Image from 'next/image';
 
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
-  DropdownMenuPortal,
   DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+  DropdownMenuShortcut
+} from '@/components/ui/dropdown-menu';
 
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "@/components/ui/tooltip";
+} from '@/components/ui/tooltip';
+import AlertDialogDemo from './Alerts';
 
 export default function DropdownMenuDemo() {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const openDialog = () => {
+    setIsDialogOpen(true);
+  };
+
+  const closeDialog = () => {
+    setIsDialogOpen(false);
+  };
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button className="outline-none  bg-transparent hover:bg-transparent focus:outline-none border-none active:border-none">
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Image
-                  src="/three_dots.svg"
-                  alt="time"
-                  width={20}
-                  height={20}
-                />
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>More task actions</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56">
-        <DropdownMenuLabel>Edit Task Action</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem className="cursor-pointer">
-            <Pencil className="mr-2 h-4 w-4" />
-            <span>Edit</span>
-            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-          </DropdownMenuItem>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button className="outline-none bg-transparent hover:bg-transparent focus:outline-none border-none active:border-none">
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Image
+                    src="/three_dots.svg"
+                    alt="time"
+                    width={20}
+                    height={20}
+                  />
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>More task actions</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>Edit Task Action</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem className="cursor-pointer">
+              <Pencil className="mr-2 h-4 w-4" />
+              <span>Edit</span>
+              <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+            </DropdownMenuItem>
 
-          <DropdownMenuItem className="cursor-pointer">
-            <Settings className="mr-2 h-4 w-4" />
-            <span>Settings</span>
-            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-          </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer">
+              <Settings className="mr-2 h-4 w-4" />
+              <span>Settings</span>
+              <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+            </DropdownMenuItem>
 
-          <DropdownMenuItem className="cursor-pointer">
-            <Image
-              alt="trash"
-              src="/task_trash.svg"
-              width={10}
-              height={10}
-              className="mr-2 h-4 w-4"
-            />
-            <span className="text-red-400">Delete</span>
-            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
+            <DropdownMenuItem onClick={openDialog} className="cursor-pointer">
+              <Image
+                alt="trash"
+                src="/task_trash.svg"
+                width={10}
+                height={10}
+                className="mr-2 h-4 w-4"
+              />
+              <span className="text-red-400">Delete</span>
+              <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
 
-        <DropdownMenuSeparator />
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <DropdownMenuSeparator />
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      {isDialogOpen && (
+        <AlertDialogDemo onClose={closeDialog} />
+      )}
+    </>
   );
 }
